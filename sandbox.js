75895baa6555f9d239d4dfccdbe08f9001b42250
Hyung Kim <hjkmines@gmail.com>
2020-10-07 20:17:02 -0600

    adds solution to get integers in range of x and y

--- sandbox.js
+++ sandbox.js
@@ -229,3 +229,21 @@ console.log(countUniqueNumbers(arraySample));
 
      return -1; 
  }
+
+//  Write a JavaScript program to get the integers in range (x, y).
+
+var range = function(start_num, end_num) 
+{
+  if (end_num - start_num === 2) 
+  {
+    return [start_num + 1];
+  } 
+  else 
+  {
+    var list = range(start_num, end_num - 1);
+    list.push(end_num - 1);
+    return list;
+  }
+};
+
+console.log(range(2,9));
