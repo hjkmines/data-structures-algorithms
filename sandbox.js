a39b6a219a38454bd8b42ee77ed39d911922bf66
Hyung Kim <hjkmines@gmail.com>
2020-10-07 20:12:01 -0600

    adds divide and conquer method for finding largest value by index

--- sandbox.js
+++ sandbox.js
@@ -206,3 +206,26 @@ console.log(countUniqueNumbers(arraySample));
 
      return max; 
  }
+
+ //Divide and conquer algorithm 
+ //Find index of specified number in array
+
+ function search(array, val) {
+     let min = 0; 
+     let max = array.length - 1; 
+
+     while (min <= max) {
+         let middle = Math.floor((min + max) / 2); 
+         let currentElement = array[middle]; 
+
+         if (array[middle] < val) {
+             min = middle + 1 
+         } else if (array[middle] > val) {
+            max = middle + 1
+         } else {
+             return middle; 
+         }
+     }
+
+     return -1; 
+ }
