bed7e968a5c69fe884612f1ac59a10f69c913ed1
Hyung Kim <hjkmines@gmail.com>
2020-10-10 11:24:34 -0600

    adds solution to quick sort

--- sandbox.js
+++ sandbox.js
@@ -376,3 +376,37 @@ const sortedLastIndex = (arr, n) => {
   };
   
   console.log(sortedLastIndex([10, 20, 30, 30, 40], 30));
+
+
+//   Write a JavaScript program to sort a list of elements using Quick sort.
+
+// Quick sort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
+
+function quick_Sort(origArray) {
+	if (origArray.length <= 1) { 
+		return origArray;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = origArray.pop();
+		var length = origArray.length;
+
+		for (var i = 0; i < length; i++) {
+			if (origArray[i] <= pivot) {
+				left.push(origArray[i]);
+			} else {
+				right.push(origArray[i]);
+			}
+		}
+
+		return newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
+	}
+}
+
+var myArray = [3, 0, 2, 5, -1, 4, 1 ];
+
+console.log("Original array: " + myArray);
+var sortedArray = quick_Sort(myArray);
+console.log("Sorted array: " + sortedArray);
